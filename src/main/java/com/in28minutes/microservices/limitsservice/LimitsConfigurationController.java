4ef09a466baa66d7a9fc3320067d3ef47e8c1a8d
juniorcapra <acaprajr@gmail.com>
2020-08-19 21:00:34 -0300

    Creating a harde coded limit service

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,33 +1,31 @@
 package com.in28minutes.microservices.limitsservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
 
-    @Autowired
-    private Configuration configuration;
+//    @Autowired
+//    private Configuration configuration;
 
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
-        LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(),
-            configuration.getMinimum());
-        return limitConfiguration;
+//        LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(),
+//            configuration.getMinimum());
+//        return limitConfiguration;
+        return new LimitConfiguration(1000,1);
     }
 
-    @GetMapping("/fault-tolerance-example")
-    @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
-    public LimitConfiguration retrieveConfiguration() {
-        throw new RuntimeException("Not available");
-    }
-
-    public LimitConfiguration fallbackRetrieveConfiguration() {
-        return new LimitConfiguration(999, 9);
-    }
+//    @GetMapping("/fault-tolerance-example")
+//    @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+//    public LimitConfiguration retrieveConfiguration() {
+//        throw new RuntimeException("Not available");
+//    }
+//
+//    public LimitConfiguration fallbackRetrieveConfiguration() {
+//        return new LimitConfiguration(999, 9);
+//    }
 
 }
