0d152a589b1fadefd5c9dce2056ed3912431bdd0
juniorcapra <acaprajr@gmail.com>
2020-08-24 20:34:08 -0300

    Connect limits service to spring cloud config server

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,21 +1,21 @@
 package com.in28minutes.microservices.limitsservice;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
-//    @Autowired
-//    private Configuration configuration;
+    @Autowired
+    private Configuration configuration;
 
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
-//        LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(),
-//            configuration.getMinimum());
-//        return limitConfiguration;
-        return new LimitConfiguration(1000,1);
+        LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(),
+            configuration.getMinimum());
+        return limitConfiguration;
     }
 
 //    @GetMapping("/fault-tolerance-example")
